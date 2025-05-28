b15c7bef1cae9ab5df53206dd47e20d465981c0e
xDanielz135XD <167827823+xDanielz135XD@users.noreply.github.com>
2025-05-28 10:12:02 -0500

    Version1Pilas

--- Master.cpp
+++ Master.cpp
@@ -4,7 +4,62 @@
 using namespace std;
 
 // ----------- Lista enlazada de procesos -----------
+struct NodoProceso {
+    int id;
+    string nombre;
+    int prioridad;
+    int tamanoMemoria;
+    string estado;
+    int memoriaAsignada;
+    NodoProceso* siguiente;
+};
 
+class ListaProcesos {
+private:
+    NodoProceso* cabeza;
+
+public:
+    ListaProcesos() : cabeza(NULL) {}
+
+    void insertar(int id, string nombre, int prioridad, int tamano) {
+        NodoProceso* nuevo = new NodoProceso{id, nombre, prioridad, tamano, "Nuevo", 0, NULL};
+        if (!cabeza) {
+            cabeza = nuevo;
+        } else {
+            NodoProceso* actual = cabeza;
+            while (actual->siguiente)
+                actual = actual->siguiente;
+            actual->siguiente = nuevo;
+        }
+        cout << "Proceso insertado correctamente.\n";
+    }
+
+    NodoProceso* buscar(int id) {
+        NodoProceso* actual = cabeza;
+        while (actual) {
+            if (actual->id == id) return actual;
+            actual = actual->siguiente;
+        }
+        return NULL;
+    }
+
+    void eliminar(int id) {
+        if (!cabeza) return;
+        if (cabeza->id == id) {
+            NodoProceso* temp = cabeza;
+            cabeza = cabeza->siguiente;
+            delete temp;
+            return;
+        }
+        NodoProceso* actual = cabeza;
+        while (actual->siguiente && actual->siguiente->id != id)
+            actual = actual->siguiente;
+        if (actual->siguiente) {
+            NodoProceso* temp = actual->siguiente;
+            actual->siguiente = temp->siguiente;
+            delete temp;
+        }
+    }
 
 
 // ----------- Pila personalizada -----------
