4be8fcfeab734904010434f1435f5210bc567ab1
Cristopher Mendez herrera <60751206@continental.edu.pe>
2025-05-27 23:33:02 -0500

    implelentacion del pilas

--- Master.cpp
+++ Master.cpp
@@ -1,5 +1,47 @@
 //Trabajo colaborativo C++ Equipo numero 04 
-//hola mundo cls
 
+// ----------- Pila personalizada -----------
+struct Nodo {
+    int id;
+    Nodo* siguiente;
+};
 
-//hola miss un gustito :3 
+class PilaMemoria {
+private:
+    Nodo* cima;
+    int maxTam;
+    int tamActual;
+
+public:
+    PilaMemoria(int limite) : cima(NULL), maxTam(limite), tamActual(0) {}
+
+    bool estaLlena() { return tamActual >= maxTam; }
+    bool estaVacia() { return cima == NULL; }
+
+    void push(int id) {
+        if (estaLlena()) {
+            cout << "Pila de memoria llena.\n";
+            return;
+        }
+        Nodo* nuevo = new Nodo{id, cima};
+        cima = nuevo;
+        tamActual++;
+    }
+
+    void pop() {
+        if (estaVacia()) return;
+        Nodo* temp = cima;
+        cima = cima->siguiente;
+        delete temp;
+        tamActual--;
+    }
+
+    void mostrar() {
+        Nodo* actual = cima;
+        cout << "Procesos en memoria (de cima a base):\n";
+        while (actual) {
+            cout << "Proceso ID: " << actual->id << endl;
+            actual = actual->siguiente;
+        }
+    }
+};
