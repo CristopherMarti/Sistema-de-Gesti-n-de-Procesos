4adbdb7611e8e25bfb2f09478bd2760d91934e32
Cristopher Mendez herrera <60751206@continental.edu.pe>
2025-05-27 23:39:35 -0500

    pilas

--- Master.cpp
+++ Master.cpp
@@ -7,50 +7,7 @@ using namespace std;
 
 
 // ----------- Pila personalizada -----------
-struct Nodo {
-    int id;
-    Nodo* siguiente;
-};
 
-class PilaMemoria {
-private:
-    Nodo* cima;
-    int maxTam;
-    int tamActual;
-
-public:
-    PilaMemoria(int limite) : cima(NULL), maxTam(limite), tamActual(0) {}
-
-    bool estaLlena() { return tamActual >= maxTam; }
-    bool estaVacia() { return cima == NULL; }
-
-    void push(int id) {
-        if (estaLlena()) {
-            cout << "Pila de memoria llena.\n";
-            return;
-        }
-        Nodo* nuevo = new Nodo{id, cima};
-        cima = nuevo;
-        tamActual++;
-    }
-
-    void pop() {
-        if (estaVacia()) return;
-        Nodo* temp = cima;
-        cima = cima->siguiente;
-        delete temp;
-        tamActual--;
-    }
-
-    void mostrar() {
-        Nodo* actual = cima;
-        cout << "Procesos en memoria (de cima a base):\n";
-        while (actual) {
-            cout << "Proceso ID: " << actual->id << endl;
-            actual = actual->siguiente;
-        }
-    }
-};
 
 // ----------- Cola personalizada -----------
 
